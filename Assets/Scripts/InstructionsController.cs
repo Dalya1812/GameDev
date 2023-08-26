4d6391da28c6160e7fc22b6f3be03cd94790dca2
Dalia <dalya1812@gmail.com>
2023-08-26 22:55:33 +0300

    adding ui

---
+++ Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsController : MonoBehaviour
+{
+    public GameObject instructionsCanvas;
+    public GameObject mazeGameObject;
+    public float instructionsDuration = 10f;
+    private void Start()
+    {
+        ShowInstructions();
+    }
+
+    private void ShowInstructions()
+    {
+        instructionsCanvas.SetActive(true);
+        mazeGameObject.SetActive(false); 
+        Invoke("HideInstructions", instructionsDuration);
+    }
+
+    private void HideInstructions()
+    {
+        instructionsCanvas.SetActive(false);
+        mazeGameObject.SetActive(true); 
+    }
+
+
+
+}
