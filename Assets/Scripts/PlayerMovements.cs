ab04607a2e2b03aa1f4cda9b455e8a176a2a6b01
Dalia <dalya1812@gmail.com>
2023-09-14 13:34:21 +0300

    update the new user input

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,51 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovements : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 5f;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-
-        rb.freezeRotation = true;
-    }
-
-    void Update()
-    {
-        HandleMovement();
-    }
-
-    private void HandleMovement()
-    {
-        float moveDirection = Input.GetAxis("Horizontal");
-        float moveDirectionY = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(moveDirection) > Mathf.Abs(moveDirectionY))
-        {
-            moveDirectionY = 0f;
-        }
-        else
-        {
-            moveDirection = 0f;
-        }
-
-        Vector2 moveVelocity = new Vector2(moveDirection * moveSpeed, moveDirectionY * moveSpeed);
-
-        Vector2 targetPosition = rb.position + moveVelocity * Time.fixedDeltaTime;
-
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, moveVelocity.normalized, moveVelocity.magnitude * Time.fixedDeltaTime);
-        if (hit.collider == null || !hit.collider.CompareTag("Maze"))
-        {
-            rb.velocity = moveVelocity;
-            rb.MovePosition(targetPosition);
-        }
-        else
-        {
-            rb.velocity = Vector2.zero;
-        }
-    }
+    
 }
